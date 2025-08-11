c82798c6d813cc8ad3921b8a776005b13e03a33a
Leanne Huang <leanne110x@gmail.com>
2025-08-11 19:53:44 +0800

    wrote our first django model

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fundraisers.apps.FundraisersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
